e9cfa8c608d81bf115e05b48c95ac4b7cf9847eb
nguyenloi99 <55385394+nguyenloi99@users.noreply.github.com>
2020-08-12 15:57:45 +0700

    del

--- hoan/src/libs/Util.java
+++
@@ -1,9 +0,0 @@
-package libs;
-
-    public class Util {
-        static  final  String Host="http://localhost:8080/cnpm123/";
-
-        public static String fullPath(String path){
-            return Host+path;
-        }
-    }
--- hoan/src/vn/edu/nlu/fit/model/user.java
+++
@@ -1,9 +0,0 @@
-package vn.edu.nlu.fit.model;
-
-public class user {
-    private String userName;
-    private String email;
-    private String password;
-    private int phone;
-    private String address;
-}
